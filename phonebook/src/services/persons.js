cd97bcad5d486eb848a3226cda3e8ba70b5ad483
JulieKihumba <eng.juliekihumba@gmail.com>
2026-01-29 15:25:43 +0300

    `Removed App, AddPopup, Header, and persons services components`

--- phonebook/src/services/persons.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
-
-//get all persons
-const getAll = () => axios.get(baseUrl);
-//add person
-// const create=(newPerson)
-
-export default { getAll };
